012dcb13f597e0c549b6dff7d4b3830f6a9a2a18
Andres Vega <aevega1991@gmail.com>
2024-12-09 21:24:01 -0300

    provisioning with user data, allowing web access to install nginx and aws-cli

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -7,8 +7,13 @@ resource "aws_instance" "web" {
   instance_type = var.instance_types.web
 
   subnet_id              = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.allow_web.id]
   key_name               = aws_key_pair.mykey.key_name
+
+  user_data = templatefile("${path.module}/templates/web.tpl", {
+    "region"     = var.aws_region
+    "bucketname" = var.bucket_name
+  })
   tags = {
     Name = "web"
   }
@@ -32,6 +37,30 @@ resource "aws_security_group" "allow_ssh" {
   ingress {
     from_port        = 22
     to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow web inbound traffic and all outbound traffic"
+  vpc_id      = module.vpc.vpc_id
+  tags = {
+    Name = "allow_web"
+  }
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 80
+    to_port          = 80
     protocol         = "TCP"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
--- first-steps/variables.tf
+++ first-steps/variables.tf
@@ -16,3 +16,8 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "bucket_name" {
+  type    = string
+  default = "test-bucket"
+}
